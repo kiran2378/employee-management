ae1ffaa1c386793edae65322e5b2e2c0bf52345a
kiran2378 <106485630+kiran2378@users.noreply.github.com>
2022-08-14 08:52:55 +0530

    Delete components directory

--- components/script.js
+++
@@ -1,8 +0,0 @@
-function showNotification() {
-   var x = document.getElementsByClassName('notification-body')
-   if (x.style.display =='none'){
-       x.style.display ='block';
-   } else {
-       x.style.display ='none'
-   }
-}
